4ec046e560b379c70951cdf8171cd87c5bc0c275
Ngozi Omenka <ngozi.madu@live.se>
2024-03-22 18:02:31 +0000

    Added products views, template and set up product for home page

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,31 @@ from django.contrib import admin
 from .models import Product, Category, Color
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Color)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'itemnumber',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('itemnumber',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class ColorAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Color, ColorAdmin)
+admin.site.register(Category, CategoryAdmin)
