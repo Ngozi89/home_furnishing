28b43ebaafb96757507425018ed91c21a9040d99
Ngozi Omenka <ngozi.madu@live.se>
2024-03-31 22:58:00 +0000

    Added confirmation email

--- home_furnishing/settings.py
+++ home_furnishing/settings.py
@@ -123,7 +123,7 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-
+DEFAULT_FROM_EMAIL = 'homefurnishing@example.com'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
