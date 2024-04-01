7b3bb2f5f17dfddb0d9e90197eaf966be2cf23f0
Ngozi Omenka <ngozi.madu@live.se>
2024-04-01 13:07:20 +0000

    Added about us page, privacy page and customer service page

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about/', views.about_page, name='about-page'),
+    path('support/', views.support_page, name='support-page'),
+    path('privacy_policy/', views.privacy_page, name='privacy-page'),
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,15 @@ def index(request):
     A view to render the home page
     '''
     return render(request, 'home/index.html')
+
+def about_page(request):
+    ''' Renders About us page '''
+    return render(request, 'home/about.html')
+
+def support_page(request):
+    ''' Renders Customer support page '''
+    return render(request, 'home/support.html')
+
+def privacy_page(request):
+    ''' Renders Privacy Policy page '''
+    return render(request, 'home/privacy_policy.html')
